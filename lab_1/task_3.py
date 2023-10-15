a0462d481485061e39cf988e03aa8df189574ac6
Sol646 <90034701+Sol646@users.noreply.github.com>
2023-10-15 17:44:24 +0300

    lab_1

---
+++ lab_1/task_3.py
@@ -0,0 +1,5 @@
+players = ["Витя", "Стас", "Петр", "Ростислав", "Миша", "Толик"]
+first_team = players[:len(players) // 2]
+second_team = players[len(players) // 2:]
+
+print(f"Первая команда: {first_team}, \nВторая команда: {second_team}")
